aaaa3195816cf5267ad9c132afe8883e6fef8ba7
kbhav77 <60478590+kbhav77@users.noreply.github.com>
2020-09-30 01:52:15 -0400

    Cell
    Set up projects and configured cell ui

---
+++ apps/app-3-starter/app-2-newsfeed/NewsItem.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+// Model for "NewsItem" goes in this file
+struct NewsItem {
+    // TODO: we need to store 1) the post title and 2) whether or not the post is favorited. One of these properties can change, and one cannot.
+    var title : String
+    var favorite : Bool
+}
